25b6737a522ac7d6a3dee784383f0843f458152d
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-11-11 17:28:53 +0200

    Add dev dependencies to poetry

---
+++ tasks.py
@@ -0,0 +1,16 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python3 src/main.py")
+
+
+@task
+def coverage(ctx):
+    ctx.run("coverage run --branch -m pytest")
+
+
+@task(coverage)
+def coverage_report(ctx):
+    ctx.run("coverage html")
