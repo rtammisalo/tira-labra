bab8b6e8118e6b72ba53e8e8f0ca7ff417437909
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-11-20 17:03:31 +0200

    Change Grid to require string encoding to have only 1 starting/goal cell

--- src/entities/grid.py
+++ src/entities/grid.py
@@ -1,3 +1,5 @@
+import re
+from collections import Counter
 from entities.cell import Cell
 
 
@@ -16,9 +18,24 @@ class Grid():
                 of the grid. Use characters '.' (empty), '#' (wall), 'S' (starting cell),
                 'G' (goal cell) to signify cell contents in the string.
         """
+        self._check_string_for_start_and_goal(grid_str)
         lines = self._clean_grid_string(grid_str)
         self._grid = [[Cell(char)
                        for char in line] for line in lines]
+
+    def _check_string_for_start_and_goal(self, grid_str):
+        matches_counter = Counter(re.findall(r'(S|G)', grid_str))
+
+        if 'S' not in matches_counter:
+            raise ValueError('No starting cell in string.')
+        if 'G' not in matches_counter:
+            raise ValueError('No goal cell in string.')
+        if matches_counter['S'] != 1:
+            raise ValueError(
+                f'Only 1 starting cell supported in string, found {matches_counter["S"]}')
+        if matches_counter['G'] != 1:
+            raise ValueError(
+                f'Only 1 goal cell supported in string, found {matches_counter["G"]}')
 
     def _clean_grid_string(self, grid_str):
         lines = []
--- src/tests/entities/grid_test.py
+++ src/tests/entities/grid_test.py
@@ -4,14 +4,10 @@ from entities.grid import Grid
 
 class TestGrid(unittest.TestCase):
     def setUp(self) -> None:
-        self.grid = Grid('##\n..\n.#\n')
-
-    def test_init_grid_with_empty_string_creates_empty_grid(self):
-        grid = Grid("")
-        self.assertEqual((grid.x_size, grid.y_size), (0, 0))
+        self.grid = Grid('S##\n...\n.G#\n')
 
     def test_init_grid_creates_corresponding_grid(self):
-        correct_grid = [['#', '#'], ['.', '.'], ['.', '#']]
+        correct_grid = ['S # #'.split(), '. . .'.split(), '. G #'.split()]
 
         for y in range(self.grid.y_size):
             for x in range(self.grid.x_size):
@@ -19,25 +15,47 @@ class TestGrid(unittest.TestCase):
                 self.assertEqual(cell, correct_grid[y][x])
 
     def test_init_grid_removes_empty_lines_from_grid_str_argument(self):
-        grid = Grid('\n\n###\n...\n')
+        grid = Grid('\n\nG##\n..S\n')
         self.assertEqual((grid.x_size, grid.y_size), (3, 2))
-        self.assertEqual([cell.cell for cell in grid[0]], '# # #'.split())
-        self.assertEqual([cell.cell for cell in grid[1]], '. . .'.split())
+        self.assertEqual([cell.cell for cell in grid[0]], 'G # #'.split())
+        self.assertEqual([cell.cell for cell in grid[1]], '. . S'.split())
 
     def test_init_grid_changes_illegal_characters_in_grid_str_to_empty_spaces(self):
-        grid = Grid('z\n#\n')
+        grid = Grid('z\n#SG\n')
         self.assertEqual(grid[0][0].cell, '.')
 
     def test_init_grid_adds_empty_spaces_to_lines_with_less_characters(self):
-        grid = Grid('...\n#\n###\n')
+        grid = Grid('SG.\n#\n###\n')
         self.assertEqual([cell.cell for cell in grid[1]], '# . .'.split())
 
     def test_grid_string_representation_is_correct(self):
-        correct_grid_str = 'Grid size of (2, 3):\n##\n..\n.#\n'
+        correct_grid_str = 'Grid size of (3, 3):\nS##\n...\n.G#\n'
         self.assertEqual(str(self.grid), correct_grid_str)
 
     def test_grid_getitem_returns_correct_grid_row_as_list(self):
-        correct_row = ['.', '.']
-        row = self.grid[1]
-        for i in range(2):
-            self.assertEqual(row[i].cell, correct_row[i])
+        correct_row = '. . .'.split()
+        row = cells_to_chars(self.grid[1])
+        self.assertEqual(row, correct_row)
+
+    def test_grid_constructor_raises_error_with_no_starting_cell(self):
+        with self.assertRaises(ValueError):
+            Grid("#..#G")
+
+    def test_grid_constructor_raises_error_with_no_goal_cell(self):
+        with self.assertRaises(ValueError):
+            Grid("#..#S")
+
+    def test_grid_constructor_raises_error_with_multiple_starting_cells(self):
+        with self.assertRaises(ValueError):
+            Grid("G#..S#SSSSS")
+
+    def test_grid_constructor_raises_error_with_multiple_goal_cells(self):
+        with self.assertRaises(ValueError):
+            Grid("GS#..#G")
+
+
+def cells_to_chars(cell_row):
+    row = []
+    for cell in cell_row:
+        row.append(cell.cell)
+    return row
