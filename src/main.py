38dde1aa0f6d01cb81d21b0f46ac3b526c2b5fb4
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-11-27 21:53:46 +0200

    UI: Add possibility to fast-forward to the end

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from ui.ui import UI
 from entities.grid import Grid
 from services.dijkstra import Dijkstra
 from services.timer_service import TimerService
+import test_maps
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "timer":
@@ -15,6 +16,6 @@ if __name__ == "__main__":
         .....S.........#..........#..###.........
         .............#........######....#...#.#.#
         """
-        test_grid = Grid(TEST_GRID_STR)
+        test_grid = Grid(test_maps.TEST_MAP)
         display = UI(test_grid, Dijkstra(test_grid))
         display.run()
--- src/ui/ui.py
+++ src/ui/ui.py
@@ -5,8 +5,8 @@ from services.dijkstra import Dijkstra
 
 
 class UI():
-    SCREEN_WIDTH = 800
-    SCREEN_HEIGHT = 600
+    SCREEN_WIDTH = 1200
+    SCREEN_HEIGHT = 830
     BACKGROUND_COLOR = (100, 100, 100)
 
     def __init__(self, grid, algorithm):
@@ -21,11 +21,12 @@ class UI():
         self._step_algorithm = algorithm.next_step()
         self._keyboard_timer = 0
         self._keys_pressed = set()
+        self._run_to_end = False
 
     def run(self):
         while True:
             self._process_input()
-            self._clock.tick(30)
+            self._clock.tick(60)
             pygame.display.flip()
 
     def _process_input(self):
@@ -33,9 +34,18 @@ class UI():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        if self._run_to_end:
+            for i in range(20):
+                self._process_next_step()
+            self._grid.draw(self._screen)
+
         key_pressed = pygame.key.get_pressed()
         if key_pressed[pygame.K_SPACE]:
             self._keys_pressed.add(pygame.K_SPACE)
+        if key_pressed[pygame.K_e]:
+            self._run_to_end = True
+        if key_pressed[pygame.K_ESCAPE]:
+            sys.exit()
 
         if self._keyboard_timer != 0:
             self._keyboard_timer -= 1
@@ -43,6 +53,7 @@ class UI():
 
         if pygame.K_SPACE in self._keys_pressed:
             self._process_next_step()
+            self._grid.draw(self._screen)
 
         self._keyboard_timer = 2
         self._keys_pressed = set()
@@ -57,5 +68,3 @@ class UI():
                 path_to_goal = stop.value
                 if path_to_goal:
                     self._grid.set_path_to_goal(path_to_goal)
-
-        self._grid.draw(self._screen)
